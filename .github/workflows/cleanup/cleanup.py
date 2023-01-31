be4d8399105dec39e9c7668fea6b0455cab94736
Liplum <Li_plum@outlook.com>
2023-01-31 17:05:29 +0800

    better clean up script.

--- .github/workflows/cleanup/cleanup.py
+++ .github/workflows/cleanup/cleanup.py
@@ -7,16 +7,34 @@ args = sys.argv
 full_name = args[1]
 owner, repo = full_name.split('/')
 package_name = owner.replace("-", "")
-main_class_source_template = """
-package %PackageName%;
+main_class_template = """package %PackageName%;
 import mindustry.mod.*;
 
-public class %MainClassName% extends Mod{
+public class %MainClassName% extends Mod {
+    
 }
+"""
+readme_template = """<div align = center>
+
+# %Repository%
+
+![Mod Icon](icon.png)
+
+</div>
+
+## Modding ToDo List:
+
+- [x] Create a new Mindustry Java mod project.
+- [ ] Edit the [mod.hjson](mod.hjson) for your mod.
+- [ ] Create your first content, a block, an item or anything you want.
+- [ ] Put sprites and bundles into [assets](assets) directory.
+- [ ] Check the Mindustry's sources with your IDE or on its [repository](https://github.com/Anuken/Mindustry).
+- [ ] Make a beautiful [mod icon](icon.png) to replace the placeholder.
+- [ ] Push a commit with any message containing `[release]` to generate a release draft on [GitHub](https://github.com/%Owner%/%Repository%/releases). 
 """
 
 
-def transform_repo_to_class(repo: str) -> str:
+def transform_repo_to_class() -> str:
     with io.StringIO() as b:
         hyphen = False
         for i, c in enumerate(repo):
@@ -37,15 +55,18 @@ def transform_repo_to_class(repo: str) -> str:
         return name
 
 
-main_class_name = transform_repo_to_class(repo) + "Mod"
+main_class_name = transform_repo_to_class() + "Mod"
 main_class_qualified_name = f"{package_name}.{main_class_name}"
 
 
 def replace_mod_meta():
-    with open("mod.hjson", mode='r') as mod_meta:
-        text = mod_meta.read()
-    text = text.replace("%Repository%", repo).replace("%Owner%", owner).replace("%MainClassQualifiedName%",
-                                                                                main_class_qualified_name)
+    text = readme_template.replace(
+        "%Repository%", repo
+    ).replace(
+        "%Owner%", owner
+    ).replace(
+        "%MainClassQualifiedName%", main_class_qualified_name
+    )
     with open("mod.hjson", mode='w') as mod_meta:
         mod_meta.write(text)
 
@@ -53,8 +74,11 @@ def replace_mod_meta():
 def generate_main_class():
     os.makedirs(f"src/{package_name}", exist_ok=True)
     with open(f"src/{package_name}/{main_class_name}.java", mode='w+') as main_clz:
-        content = main_class_source_template.replace("%PackageName%", package_name).replace("%MainClassName%",
-                                                                                            main_class_name)
+        content = main_class_template.replace(
+            "%PackageName%", package_name
+        ).replace(
+            "%MainClassName%", main_class_name
+        )
         main_clz.write(content)
 
 
